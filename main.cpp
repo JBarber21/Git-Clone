35c70c4b9b284b4e243f66aa23b06358a4af8e82
JBarber21 <Jonathon2997@gmail.com>
2026-07-29 17:04:28 -0500

    Abstracting Init
    Separating the logic of the init function from main and putting it in its own function and file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <filesystem>
-#include <fstream>
+#include "init.hpp"
 using namespace std;
 
 int main(int argc, char* argv[]){
@@ -12,31 +11,9 @@ int main(int argc, char* argv[]){
     
     std::string arg = argv[1];
 
-    if(arg == "init"){
-        //std::string dir = argv[2];
-        if(argv[2] == NULL){
-            std::cout << "Cannot Create NULL Directory";
-            return 0; 
-        }
-        std::string dir = argv[2];
+    if(arg == "init") {init(argv[2]);}
 
-        if(std::filesystem::exists(dir) == true){
-            std::cout << "Directory Already Exists";
-        }
-        
-        std:filesystem::create_directory(dir);
-        std::filesystem::create_directory( dir + "/.git");
-        std::ofstream HEAD( dir + "/.git/HEAD.txt");
-        HEAD << "refs/heads/master";
-        HEAD.close();
-        std::filesystem::create_directory( dir + "/.git/objects");
-        std::filesystem::create_directory( dir + "/.git/refs");
-        std::filesystem::create_directory( dir + "/.git/refs/heads");
-        
-    }
-    else{
-        std::cout << "Not a valid command";
-    }
+    else{std::cout << "Not a valid command";}
 
     return 0;
 }
